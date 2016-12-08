ad392255085828f6d86892875217810d53888268
jatin <getjatin2010@live.com>
2016-12-08 20:14:36 +0530

    Nothing

--- DatabaseConnection/config.php
+++ DatabaseConnection/config.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "localhost";
-$username = "epicharyana";
-$password = "epicharyana123";
+$username = "root";
+$password = "fasttracK";
 $dbname = "epicharyanadb";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
