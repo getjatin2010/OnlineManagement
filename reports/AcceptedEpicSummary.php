728c6ddad2a3945be2998af1a11e46d63799c0e8
jatin <getjatin2010@live.com>
2016-12-11 01:32:04 +0530

    Done Removing Errors

--- reports/AcceptedEpicSummary.php
+++ reports/AcceptedEpicSummary.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+error_reporting(E_ERROR | E_PARSE);
 require('../fpdf181/fpdf.php');
 include('../UtilityFiles/checkLogin.php');
 
--- reports/createReport.php
+++ reports/createReport.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+error_reporting(E_ERROR | E_PARSE);
 include('../UtilityFiles/checkLogin.php');
 $var = check_login_func();
 
